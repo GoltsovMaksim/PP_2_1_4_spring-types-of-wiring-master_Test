c060ee378511785a538ee0b6e62769ab8795c5af
Maksim Goltsov <mgoltsov@bk.ru>
2022-10-20 18:42:08 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,13 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+    public Deth8() {
+    }
+        @Override
+    public String toString() {
+        return "...Death...";
+    }
+}
---
+++ src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Egg6 {
+    private final Needle7 neddle;
+    @Autowired
+    public Egg6(Needle7 neddle) {
+        this.neddle = neddle;
+    }
+
+    @Override
+    public String toString() {
+        return ", в яйце иголка " + neddle.toString();
+    }
+}
